5900d505ac7df7f999e1f1b97e8562b4444789a5
crisCastroG <cris.castrog@duocuc.cl>
2024-06-14 22:48:00 -0400

    Se agrego modelo clase de producto

---
+++ src/modelo/Producto.java
@@ -0,0 +1,43 @@
+
+package modelo;
+
+public class Producto {
+    private int cod_prod;
+    private String nombre_prod;
+    private int precio_prod;
+
+    public Producto() {
+    }
+
+    public Producto(int cod_prod, String nombre_prod, int precio_prod) {
+        this.cod_prod = cod_prod;
+        this.nombre_prod = nombre_prod;
+        this.precio_prod = precio_prod;
+    }
+
+    public int getCod_prod() {
+        return cod_prod;
+    }
+
+    public String getNombre_prod() {
+        return nombre_prod;
+    }
+
+    public int getPrecio_prod() {
+        return precio_prod;
+    }
+
+    public void setCod_prod(int cod_prod) {
+        this.cod_prod = cod_prod;
+    }
+
+    public void setNombre_prod(String nombre_prod) {
+        this.nombre_prod = nombre_prod;
+    }
+
+    public void setPrecio_prod(int precio_prod) {
+        this.precio_prod = precio_prod;
+    }
+    
+    
+}
