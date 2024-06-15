c478ded0ca599eaa42aa732f17f2daa8d493fdd5
cristianjcastro <chriscg_95@hotmail.com>
2024-06-14 21:30:41 -0400

    Primer commit

---
+++ src/db/Conexion.java
@@ -0,0 +1,24 @@
+
+package db;
+
+import java.sql.*;
+
+public class Conexion {
+     public static Connection getConexion() {
+
+        Connection connection = null;
+        try {
+            String bbdd_mysql = "employeedb";
+            String user_mysql = "root";
+            String pass_mysql = "";
+            String driverClassName = "com.mysql.jdbc.Driver";
+            String driverUrl="jdbc:mysql://localhost/" + bbdd_mysql;
+            Class.forName(driverClassName);
+            connection = DriverManager.getConnection(driverUrl, user_mysql,pass_mysql);
+            System.out.println("Conexión exitosa");         
+        }catch (Exception e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
